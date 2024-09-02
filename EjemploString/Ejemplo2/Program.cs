b7ae21de272e61166d80948cd367784af6beb88d
BenjaSimon <benjasimon2001@gmail.com>
2024-09-02 06:32:00 -0300

    Primera subida

---
+++ EjemploString/Ejemplo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre  = "Pedro";
+            nombre = "Hola señor " + nombre;
+            int cantC = nombre.Length;
+
+            nombre = nombre.Replace("Hola" , "Chau");
+            
+            
+            Console.WriteLine(nombre);
+            Console.ReadKey();
+        }
+    }
+}
